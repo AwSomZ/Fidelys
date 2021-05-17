667b8a1744fe00e1b3bf77788c26ac02031cfecd
AwSomZ <awsomz21@gmail.com>
2021-05-17 23:52:17 +0100

    Add files via upload

--- fidelysapi/reclamation.php
+++ fidelysapi/reclamation.php
@@ -5,9 +5,11 @@
             $client=$_POST['client'];
             $titre=$_POST['titre'];
             $description=$_POST['description'];
+            $datecreation = date("Y-m-d");
+            $etat="en cours";
             
-            $sql="insert into  reclamation (titre, description, client) VALUES
-            ('$titre','$description','$client');";
+            $sql="insert into  reclamation (titre, description, client, datecreation, etat) VALUES
+            ('$titre','$description','$client','$datecreation','$etat');";
             $res=$bd->exec($sql);
            
         }
